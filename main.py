edffd072aa0dad74ee05b0e58db3775da017c0b3
Sinchana-patgar <sinchanapatgar24@gmail.com>
2026-06-18 23:08:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from groq import Groq
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-MODEL_NAME = os.getenv("MODEL_NAME", "llama3-8b-8192")
+MODEL_NAME = os.getenv("MODEL_NAME", "llama-3.3-70b-versatile")
 
 def extract_text(file):
     if file.name.endswith(".pdf"):
